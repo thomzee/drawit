9033fd4e319ccfde9a771a4956ddde10a74ae827
root <root@thomzee.asia-southeast2-a.c.thomzee.internal>
2020-07-07 19:35:16 +0000

    fix error

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
 var server = app.listen(3000);
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 console.log('Server is running...');
 
 var socket = require('socket.io');
 
 var io = socket(server);
+
 io.sockets.on('connection', newConnection);
 
 function newConnection(socket) {
